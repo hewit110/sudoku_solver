d1cd43dd7c7d22e1ba401b461c12e8fc57c315c8
Kevin Hewitt <hewit110@mail.chapman.edu>
2017-03-09 21:09:53 -0800

    Updated thread functionality to work from main

--- main.java
+++ main.java
@@ -1,17 +1,65 @@
-
-import java.util.Scanner;
-import java.io.*;
 public class main
 {
 
     public static void main(String args[])
     {
         solver sudokusolver = new solver();
-        solver.readFile();
+        sudokusolver.readFile();
 
-        rowThreads t1 = new rowThreads();
-        gridThreads t2 = new gridThreads();
-        solver.printPuzzle();
+        Thread t1 = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                sudokusolver.checkColumns(0,0);
+            }
+        };
+
+        Thread t2 = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                sudokusolver.checkRows(0,0);
+            }
+        };
+
+        Thread t3 = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                sudokusolver.check3x3(0, 0);
+                sudokusolver.check3x3(3, 0);
+                sudokusolver.check3x3(6, 0);
+                sudokusolver.check3x3(0, 3);
+                sudokusolver.check3x3(3, 3);
+                sudokusolver.check3x3(6, 3);
+                sudokusolver.check3x3(0, 6);
+                sudokusolver.check3x3(3, 6);
+                sudokusolver.check3x3(6, 6);
+            }
+        };
+
+        t1.start();
+        t2.start();
+
+        try{
+        t3.join();
+        }
+        catch (Exception e) {
+            System.out.println("Something occurred with threads 1 or 2");
+            e.printStackTrace();
+        }
+        try{
+            t3.join();
+        }
+        catch (Exception e) {
+            System.out.println("Something occurred with thread 3");
+            e.printStackTrace();
+        }
+
+        sudokusolver.printPuzzle();
     }
 
 }
--- solver.java
+++ solver.java
@@ -9,11 +9,25 @@ import java.util.Scanner;
 
 
 public class solver {
+    //global puzzle value to allow multiple threads to change it
 
     public  static int[][] puzzle = new int[9][9];
 
-    public static void readFile()
+    public solver()
     {
+
+    }
+
+
+    public static void readFile()
+    /*
+    Reads a .txt file into a string one line at a time
+    The string reads into a 2d array
+    A new line in the .txt file trips a "row" counter to begin the new line in the array
+    */
+    {
+
+
         Scanner scan = null;try {
             String inputLine = "";
             int row = 0;
@@ -33,7 +47,9 @@ public class solver {
                 }
                 row++;
             }
+            System.out.println("Text file read successfully!");
         }catch (FileNotFoundException e) {
+            System.out.println("The file was not found");
             e.printStackTrace();
         } catch (Exception e) {
             System.out.println("The following error accurred" + e);
@@ -41,6 +57,13 @@ public class solver {
     }
 
     public static void check3x3(int x, int y)
+    /*
+    Runs through the array one value at a time
+    After a value is encountered, run through the array one element at a time beginning with the element in front of it
+    Check both values against eachother
+    If a duplicate is found, run a function to generate the replacement
+        Set array element equal to replacement value
+    */
     {
         for (int i = x; i < x + 3; ++i)
         {
@@ -53,7 +76,11 @@ public class solver {
                     {
                         if (puzzle[i][j] == puzzle[l][k])
                         {
-                            puzzle[i][j] = replacement(x,y);
+                            int newNum = replacement(x,y);
+                            System.out.println("An error was found at [" + i + "],[" + j + "] " +
+                                    " in the grid [" + x + "],[" + y + "] " +
+                                    "and will be replaced with " + newNum);
+                            puzzle[i][j] = newNum;
                         }
                     }
                 }
@@ -62,9 +89,15 @@ public class solver {
 
     }
 
-    public static void checkRows(int x)
+    public static void checkRows(int completed, int x)
+    /*
+    Checks row x for duplicates by running through the array
+    All elements are checked against the remaining elements of the array starting with the beginning of the row
+    If a duplicate is found, the first duplicate value is swapped with the value on the row below it
+    If a duplicate is found in the final row, it is swapped with the corresponding value in the first row
+    Function runs recursively until all rows have been checked without errors
+    */
     {
-        int completed = 0;
 
         for (int i = 0; i <9; ++i)
         {
@@ -73,7 +106,8 @@ public class solver {
 
                 if (puzzle[x][i] == puzzle [x][j])
                 {
-
+                    System.out.println("A duplicate value was found at [" + x + "],[" + i + "] " +
+                            "and will swap places with the value below it");
                     if (x == 8)
                     {
                         int temp = puzzle[x][i];
@@ -87,18 +121,28 @@ public class solver {
                         puzzle[x][i] = puzzle[x+1][i];
                         puzzle[x+1][i] = temp;
                     }
-                    completed--;
+                    //if an error is found, reset the counter to 0
+                    completed = 0;
                 }
-                completed++;
             }
         }
 
-        if (completed != 8)
+        completed++;
+
+
+        /*
+        NOTE: "completed" will always be incremented by one even after the counter is reset
+        NOTE: to counteract this, I check for 9 iterations rather than 8 (the index size of a sudoku board)
+        If not enough iterations have been completed, check the next row in the puzzle array
+        If the row being check was the final row, reset the counter to start checking the beginning row
+         */
+
+        if (completed != 9)
         {
             if (x == 8) {
-                checkRows(0);
+                x = 0;
             } else {
-                checkRows(++x);
+                checkRows(completed, x++);
             }
         }
         else
@@ -106,13 +150,17 @@ public class solver {
             System.out.println("Vertical lines have been validated and are complete");
         }
 
-
     }
 
-    public static void checkColumns(int x)
+    public static void checkColumns(int completed, int x)
+    /*
+    Checks column x for duplicates by running through the array
+    All elements are checked against the remaining elements of the array starting with the beginning of the column
+    If a duplicate is found, the first duplicate value is swapped with the value on the column next to it
+    If a duplicate is found in the final column, it is swapped with the corresponding value in the first column
+    Function runs recursively until all column have been checked without errors
+    */
     {
-        int completed = 0;
-
 
         for (int i = 0; i <9; ++i)
         {
@@ -124,6 +172,8 @@ public class solver {
 
                     if (x == 8)
                     {
+                        System.out.println("A duplicate value was found at [" + i + "],[" + x + "] " +
+                                "and will swap places with the value beside it");
                         int temp = puzzle[i][x];
                         puzzle[i][8] = puzzle[i][0];
                         puzzle[i][0] = temp;
@@ -135,34 +185,50 @@ public class solver {
                         puzzle[i][x] = puzzle[i][x+1];
                         puzzle[i][x+1] = temp;
                     }
-                    completed--;
+                    completed = 0;
                 }
-                completed++;
+
             }
         }
+        completed++;
 
-        if (completed != 8)
-        {
-            if (x == 8) {
-                checkRows(0);
-            } else {
-                checkRows(++x);
+        /*
+        NOTE: "completed" will always be incremented by one even after the counter is reset
+        NOTE: to counteract this, I check for 9 iterations rather than 8 (the index size of a sudoku board)
+        If not enough iterations have been completed, check the next row in the puzzle array
+        If the row being check was the final row, reset the counter to start checking the beginning row
+         */
+            if (completed != 9)
+            {
+                if (x == 8) {
+                    x = 0;
+                } else {
+                    checkColumns(completed, x++);
+                }
             }
-        }
-        else
-        {
-            System.out.println("Horizontal lines have been validated and are complete");
+            else
+            {
+                System.out.println("Horizontal columns have been validated and are complete");
+            }
+
         }
 
-
-    }
 
     public static int replacement(int x, int y)
+    /*
+    Calculates a replacement value for a 3x3 grid
+    takes in 3x3 beginning coordinates as parameters
+    checks corresponding 3x3 for a 1 - 9 value that is NOT currently used by the 3x3
+    Returns the value to be the replacement for a duplicate
+     */
     {
+        //counter acts as the value that is being checked for in the 3x3
         int counter = 1;
         boolean valueMatch = false;
         int replacement = 0;
 
+        //check for a value = to counter
+        //if one is found, do nothing
         for (int k = 0; k < 9; ++k) {
             for (int i = x; i < x + 3; ++i) {
                 for (int j = y; j < y + 3; ++j) {
@@ -171,6 +237,9 @@ public class solver {
                     }
                 }
             }
+            //if a cycle goes through the 3x3 without encountering a match;
+            //return the value of the counter as the replacement
+            //otherwise, check for the next value by incrementing "counter"
             if (!valueMatch) {
                 replacement = counter;
             }
